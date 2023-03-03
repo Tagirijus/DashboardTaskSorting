2ec53afa11e794098639d68c03c483f6bcae19c7
Manuel <info@tagirijus.de>
2023-03-03 21:56:36 +0100

    Removed comment

--- Pagination/TagiDashboardPaginationTaskSorting.php
+++ Pagination/TagiDashboardPaginationTaskSorting.php
@@ -68,7 +68,6 @@ class TagiDashboardPaginationTaskSorting extends Base
                 ->setUrl('DashboardController', 'show', array('user_id' => $userId, 'pagination' => 'tasks-'.$projectId), 'project-tasks-'.$projectId)
                 ->setMax(15)
                 ->setOrder(TaskModel::TABLE.'.date_due')
-                // ->setDirection('ASC')
                 ->setFormatter($this->taskListSubtaskAssigneeFormatter->withUserId($userId))
                 ->setQuery($query)
                 ->calculateOnlyIf($this->request->getStringParam('pagination') === 'tasks-'.$projectId);
